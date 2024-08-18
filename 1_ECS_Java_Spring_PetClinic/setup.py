4168c60874c8c98166ebfe5444830af111376622
Musalinux <55594624+Musalinux@users.noreply.github.com>
2024-08-18 02:29:17 +0100

    Update setup.py

--- 1_ECS_Java_Spring_PetClinic/setup.py
+++ 1_ECS_Java_Spring_PetClinic/setup.py
@@ -55,78 +55,33 @@ def create_ecs_cluster(stack_name):
 
     describe_images_response = ec2_client.describe_images(
         DryRun=False,
-        Owners=[
-            'amazon',
-        ],
-        Filters=[
-            {
-                'Name': 'name',
-                'Values': [
-                    'amzn-ami-2016.09.f-amazon-ecs-optimized',
-                ]
-            },
-        ]
+        Owners=['amazon'],
+        Filters=[{'Name': 'name', 'Values': ['amzn-ami-2016.09.f-amazon-ecs-optimized']}]
     )
-    try:
-        ec2_client.create_key_pair(
-            DryRun=False,
-            KeyName=stack_name + 'key'
-        )
-    except Exception as e:
-        pass
+
+    if not describe_images_response['Images']:
+        raise Exception("No suitable AMI found")
+
+    ami_id = describe_images_response['Images'][0]['ImageId']
 
     try:
         response = cf_client.create_stack(
             StackName=stack_name,
             TemplateBody=json.dumps(cloudformation_json),
             Parameters=[
-                {
-                    'ParameterKey': 'AsgMaxSize',
-                    'ParameterValue': '2',
-                    'UsePreviousValue': True
-                },
-                {
-                    'ParameterKey': 'EcsAmiId',
-                    'ParameterValue': describe_images_response['Images'][0]['ImageId'],
-                    'UsePreviousValue': True
-                },
-                {
-                    'ParameterKey': 'EcsClusterName',
-                    'ParameterValue': stack_name,
-                    'UsePreviousValue': True
-                },
-                {
-                    'ParameterKey': 'KeyName',
-                    'ParameterValue': stack_name + 'key',
-                    'UsePreviousValue': True
-                },
-                {
-                    'ParameterKey': 'EcsInstanceType',
-                    'ParameterValue': 'm4.large',
-                    'UsePreviousValue': True
-                },
-                {
-                    'ParameterKey': 'DBUsername',
-                    'ParameterValue': 'PetClinicDB'
-                },
-                {
-                    'ParameterKey': 'DBPassword',
-                    'ParameterValue': 'PetClinicPassw0rd'
-                }
+                {'ParameterKey': 'AsgMaxSize', 'ParameterValue': '2', 'UsePreviousValue': True},
+                {'ParameterKey': 'EcsAmiId', 'ParameterValue': ami_id, 'UsePreviousValue': True},
+                {'ParameterKey': 'EcsClusterName', 'ParameterValue': stack_name, 'UsePreviousValue': True},
+                {'ParameterKey': 'KeyName', 'ParameterValue': stack_name + 'key', 'UsePreviousValue': True},
+                {'ParameterKey': 'EcsInstanceType', 'ParameterValue': 'm4.large', 'UsePreviousValue': True},
+                {'ParameterKey': 'DBUsername', 'ParameterValue': 'PetClinicDB'},
+                {'ParameterKey': 'DBPassword', 'ParameterValue': 'PetClinicPassw0rd'}
             ],
             TimeoutInMinutes=123,
-            Capabilities=[
-                'CAPABILITY_IAM',
-            ],
+            Capabilities=['CAPABILITY_IAM'],
             OnFailure='DELETE',
-            Tags=[
-                {
-                    'Key': 'Name',
-                    'Value': stack_name
-                },
-            ]
+            Tags=[{'Key': 'Name', 'Value': stack_name}]
         )
-
     except cf_client.exceptions.AlreadyExistsException:
         logger.warning("CF Stack already exists")
         pass
@@ -135,6 +90,7 @@ def create_ecs_cluster(stack_name):
 
     resources = cf_client.describe_stack_resources(StackName=stack_name)
     return resources
+
 
 
 def create_ecs_cluster_mysql(stack_name, stack_name_ecs_cluster, vpc_id, subnet1, subnet2, role_arns, region):
